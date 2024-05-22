4c44a0e7ba22421c3cb4c2a852476a3b38e6e0d0
Рустам <imamiev@mail.ru>
2024-05-23 08:50:59 +0900

    5.2 -Добавит сортировку.     -Свяжет раздел инфо с реальными точками -Рефакторинг View и модели

--- src/view/destination-points-view.js
+++ src/view/destination-points-view.js
@@ -1,10 +1,17 @@
 import AbstractView from '../framework/view/abstract-view';
+import { render } from '..//framework/render';
 
 const createTripPointListTemplate = () => `
   <ul class="trip-events__list">
-  </ul>`;
+  </ul>
+`;
 
 export default class DestinationPointsView extends AbstractView {
+  constructor ({ container}) {
+    super();
+    render(this, container);
+  }
+
   get template() {
     return createTripPointListTemplate();
   }
